8c54ed3569e44fc4def2fba639dccccd1df147b4
SladeBennett <slade8colt@gmail.com>
2024-08-24 15:18:43 -0600

    GET(tasks) optimal

--- api/task/model.js
+++ api/task/model.js
@@ -2,6 +2,15 @@ const db = require('../../data/dbConfig')
 
 async function getTasks() {
     const taskRows = await db('tasks as t')
+    .join('projects as p', 't.project_id', 'p.project_id')
+    .select(
+        't.task_id',
+        't.task_description',
+        't.task_notes',
+        't.task_completed',
+        'p.project_name',
+        'p.project_description'
+    )
     return taskRows
 }
 
