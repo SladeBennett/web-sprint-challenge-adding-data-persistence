49140c2fd907018466d2697b503446a3f2b48f9d
SladeBennett <slade8colt@gmail.com>
2024-08-24 15:37:09 -0600

    GET&&POST(resources) optimal

--- api/resource/model.js
+++ api/resource/model.js
@@ -5,6 +5,14 @@ async function getResources() {
     return resourceRows
 }
 
+async function addNewRes(resource) {
+    return db('resources').insert(resource)
+    .then(([resource_id]) => {
+        return db('resources').where('resource_id', resource_id).first()
+    })
+}
+
 module.exports = {
     getResources,
+    addNewRes,
 }
--- api/resource/router.js
+++ api/resource/router.js
@@ -9,6 +9,15 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
+router.post('/', (req, res, next) => {
+    const resource = req.body
+    Resources.addNewRes(resource)
+    .then(resource => {
+        res.status(201).json(resource)
+    })
+    .catch(next)
+})
+
 router.use((err, req, res, next) => { //eslint-disable-line
     res.status(500).json({
         customMessage: 'Chaos strikes again in Resource/Router',
